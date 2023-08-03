9af9bd40b29bec59dcd240c91d313a5c2e3ff931
Hashir <hashirabbasi112@gmail.com>
2023-08-03 09:46:07 -0400

    404

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
       <div class="lg:pr-8 lg:pt-4">
         <div class="lg:max-w-lg">
-          <h2 class="text-base font-semibold leading-7 text-[#c2b669]">Attendace, made faster</h2>
+          <h2 class="text-base font-semibold leading-7 text-[#c2b669]">Attendance, made faster</h2>
           <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Club Attendance</p>
           <p class="mt-6 text-lg leading-8 text-gray-600">Club attendance has never been easier! Just scan the QR code provided by the club president and login with your school email to log your attendance.</p>
         </div>
