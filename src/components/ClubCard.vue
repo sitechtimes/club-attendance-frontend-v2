adab525bce3442c8ea9446afba74f5d9d418d6c5
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2023-08-03 08:31:33 -0400

    s

--- src/components/ClubCard.vue
+++ src/components/ClubCard.vue
@@ -1,8 +1,11 @@
 <template>
-
+  <div class="w-[90%] h-[14vh] border-2 rounded-lg border-black mx-auto my-6">
+    <h2 class="text-xl border-b-2 p-2">Anime Club</h2>
+    <div class="w-full h-[15%] justify-start flex items-center p-2">
+      <div class="w-[30%] text-md font-medium pt-6">President</div>
+      <div class="w-[70%] text-md font-medium pt-6"># of Members</div>
+    </div>
+  </div>
 </template>
 
-
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,8 +1,21 @@
 <template>
-
+  <section class="w-screen h-screen">
+    <div class="w-full h-[15%] border-b-2 justify-center flex items-center">
+      <div class="w-[32%] text-lg font-medium">Administration</div>
+      <div class="w-[5%]">🔎</div>
+      <input
+        class="w-[55%] border-2 border-black h-[35%] rounded-full pl-2 ml-2"
+        placeholder="Search"
+      />
+    </div>
+    <div class="w-full h-auto justify-center flex-col items-center">
+      <ClubCard></ClubCard>
+      <ClubCard></ClubCard>
+      <ClubCard></ClubCard>
+    </div>
+  </section>
 </template>
 
-
 <script setup lang="ts">
-
+import ClubCard from "@/components/ClubCard.vue";
 </script>
