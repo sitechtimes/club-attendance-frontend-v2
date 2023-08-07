a3f5ee0e1e1017f7bd730741be1ad82e64781ad0
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2023-08-07 12:21:47 -0400

    asdf

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -13,24 +13,26 @@
 <script setup lang="ts">
 import AdminClubCard from "@/components/AdminClubCard.vue";
 import SearchBar from "@/components/SearchBar.vue";
+import { useUserStore } from "@/stores/user";
 
-async function getData(url = "") {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: "GET", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
+const store = useUserStore();
 
-getData("http://localhost:3000/getAllClubData").then((data) => {
-  console.log(data); // JSON data parsed by `data.json()` call
+console.log(store.user, "user");
+
+// async function getData(url = "") {
+//   const response = await fetch(url, {
+//     method: "GET",
+//     mode: "cors",
+//     cache: "no-cache",
+//     credentials: "same-origin",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     redirect: "follow",
+//   });
+//   return response.json();
+// }
+
+// getData("http://localhost:3000/getAllClubData").then((data) => {
+//   console.log(data);
 </script>
