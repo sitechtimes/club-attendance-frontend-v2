b1637909d41e5eaae7a054abcb62ac9c0465a792
kevinc2465 <chenk5104@gmail.com>
2023-09-25 21:56:19 -0400

    added css to the button

--- src/components/ChangeDate.vue
+++ src/components/ChangeDate.vue
@@ -2,7 +2,12 @@
   <div>
     <vue-changedate>
       <input type="text" placeholder="New Date" />
-      <button @click="handleSubmit">Change date</button>
+      <button
+        class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-14 sm:w-auto"
+        @click="handleSubmit"
+      >
+        Change date
+      </button>
     </vue-changedate>
   </div>
 </template>
