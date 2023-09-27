2d93362debe078d45de80c620923adaa1af673ff
kevinc2465 <chenk5104@gmail.com>
2023-09-27 10:55:35 -0400

    trying to figure out the button

--- src/components/ChangeDate.vue
+++ src/components/ChangeDate.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
     <vue-changedate>
-      <input type="text" placeholder="New Date" />
+      <!-- <div
+        id="datepicker"
+        class="input-group date"
+        data-date-format="mm-dd-yyyy"
+      >
+        <input
+          class="form-control"
+          type="text"
+          placeholder="New Date"
+          readonly
+        /> -->
+      <!-- <span class="input-group-addon"
+          ><i class="glyphicon glyphicon-calendar"></i
+        ></span> -->
+      <!-- </div> -->
+      <input type="text" class="nextDate" placeholder="New Date" />
       <button
         class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-14 sm:w-auto"
         @click="handleSubmit"
@@ -22,7 +37,7 @@ const store = useClubStore();
 const nextMeeting = ref("");
 
 onMounted(() => {
-  console.log(store.date);
+  console.log(input.value);
 });
 const handleSubmit = () => {
   store.date = nextMeeting.value;
--- src/stores/club.ts
+++ src/stores/club.ts
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia';
    export const useClubStore = defineStore('club', {
       state: () => ({
           user:'',
-          date: '12',
+          date: '1',
         }),
   });
+
