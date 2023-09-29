5480816cd2a1d9be71877d535ae0cef5dead9f0b
kevinc2465 <chenk5104@gmail.com>
2023-09-29 10:57:07 -0400

    crying

--- src/components/ChangeDate.vue
+++ src/components/ChangeDate.vue
@@ -3,9 +3,9 @@
     <vue-changedate>
       <input
         type="text"
-        class="nextDate"
+        id="nextDate"
         placeholder="New Date"
-        v-model="nameInput"
+        v-model="changeDate"
       />
       <button
         class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-14 sm:w-auto"
@@ -20,14 +20,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useClubStore } from "../stores/club";
-import { nextMeeting } from "./ClubCard.vue";
-import { onMounted } from "vue";
 
 const nameInput = ref("");
-const nextMeeting = useClubStore.date;
+const store = useClubStore;
 
 const handleSubmit = () => {
-  nameInput.value = nextMeeting.value;
+  // store.changeName(nameInput.value);
+  return console.log(nameInput.value);
   // useClubStore.changeName(nameInput.value);
   // nameInput.value = nextMeeting.value;
   // nextMeeting.value = "";
