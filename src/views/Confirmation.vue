35e6ed7b26a64709979552670990e06c8bc2f616
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-10-12 11:00:44 -0400

    confirmation page *rough* skeleton

--- src/views/Confirmation.vue
+++ src/views/Confirmation.vue
@@ -1,8 +1,18 @@
 <template>
-<h1>Wow this is a cool attendance confirmation page</h1>
-<button>Click Here</button>
+<div>
+    <h1>Confirm Your Attendance</h1>
+<button
+class="p-4 text-[#c2b669] shadow-xl cursor-pointer rounded-md hover:scale-110 ease-in-out duration-300 bg-black w-36 h-18 flex justify-evenly items-center"
+@click="logAttendance">
+ Click Here
+</button>
+</div>
 </template>
 
 <script setup lang="ts">
-
+import { onMounted, onBeforeMount, ref, computed, watch } from "vue";
+let present = ref(false)
+const logAttendance = function(){
+present.value = true 
+}
 </script>
