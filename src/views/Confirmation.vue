1ffc5b21d49a56ac04dbc7e4e33b61de8f44cbd1
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-10-17 10:33:35 -0400

    sad

--- src/views/Confirmation.vue
+++ src/views/Confirmation.vue
@@ -18,16 +18,14 @@ class="p-4 text-[#c2b669] shadow-xl cursor-pointer rounded-md hover:scale-110 ea
 <script setup lang="ts">
 import { onMounted, onBeforeMount, ref, computed, watch } from "vue";
 import { useUserStore } from "@/stores/user";
-import { useClubStore } from "@/stores/club";
+//import { useClubStore } from "@/stores/club";
 const userStore = useUserStore();
-const clubStore = useClubStore();
-console.log(clubStore.count)
+//const clubStore = useClubStore();
 let present = ref(false)
 const logAttendance = function(){
 present.value = true 
-clubStore.increment();
 }
 onMounted(()=>{
-clubStore.getData();
+//clubStore.getData();
 })
 </script>
