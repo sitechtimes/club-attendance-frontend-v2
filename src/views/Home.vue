f862141b096a9c40813c1fc7cd9b9a38ea0e1193
kevinc2465 <chenk5104@gmail.com>
2023-10-24 22:26:43 -0400

    cooking?

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="overflow-hidden bg-white pt-20 lg:pt-24">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8 2xl:ml-40">
+  <div
+    class="overflow-hidden py-80 bg-white pt-20 lg:pt-24 bg-yellow -rotate-12"
+  >
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 2xl:ml-40 skew-x-12">
       <div
         class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2"
       >
-        <div class="lg:pr-8 lg:pt-4">
+        <div class="lg:pr-8 lg:pt-4 rotate-12 -skew-x-12">
           <div class="lg:max-w-lg">
             <h2 class="text-base font-semibold leading-7 text-[#c2b669]">
               Attendance, made faster
@@ -26,13 +28,15 @@
             <Calender />
           </div>
         </div>
-        <img
-          src="@/assets/sammy.jpg"
-          alt="screenshot"
-          class="w-[28rem] sm:w-[50rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 lg:w-[45rem] xl:w-[50rem] 2xl:w-[70rem] md:-ml-4 lg:-ml-0"
-        />
       </div>
     </div>
+  </div>
+  <div class="bg-black">
+    <img
+      src="@/assets/sammy.jpg"
+      alt="screenshot"
+      class="w-[28rem] sm:w-[50rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 lg:w-[45rem] xl:w-[50rem] 2xl:w-[70rem] md:-ml-4 lg:-ml-0"
+    />
   </div>
 </template>
 
