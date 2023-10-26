f4c3734b0b6601da2539303924ae6ac5ae4f778d
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-10-26 10:54:49 -0400

    confirmation page basic css

--- src/views/Confirmation.vue
+++ src/views/Confirmation.vue
@@ -1,17 +1,29 @@
 <template>
-<div v-if="!present">
-    <h1>Confirm Your Attendance</h1>
-<button 
-class="p-4 text-[#c2b669] shadow-xl cursor-pointer rounded-md hover:scale-110 ease-in-out duration-300 bg-black w-36 h-18 flex justify-evenly items-center"
-@click="logAttendance">
- Click Here
-</button>
-</div>
-<div v-else-if="present">
-    <p>Thank you for confirming.</p>
-</div>
-<div v-else>
- <p>Error</p>
+<div class="overflow-hidden bg-white pt-20 lg:pt-24">
+    <div v-if="!present"
+    class = "flex flex-col items-center justify-center">
+        <h1 
+        class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+         Please Confirm Your Attendance
+        </h1>
+        <div
+        class="flex flex-col mt-8">
+            <button 
+            class="p-4 text-[#c2b669] shadow-xl cursor-pointer rounded-md hover:scale-110 ease-in-out duration-300 bg-black w-36 h-18 flex justify-evenly items-center"
+            @click="logAttendance">
+             Click Here
+            </button>
+        </div>
+    </div>
+    <div v-else-if="present"
+    class="flex flex-col items-center justify-center">
+        <h1 
+        class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        Thank You!</h1>
+    </div>
+    <div v-else>
+        <p>Error</p>
+    </div>
 </div>
 </template>
 
