4fbcac30cc1cab34f84ec1945ddddb073297341e
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2023-10-31 10:34:03 -0400

    s

--- src/stores/club.ts
+++ src/stores/club.ts
@@ -5,24 +5,6 @@ import axios from "axios";
 
 const api = mande("http://localhost:3000/getAllClubData");
 
-// export const useClubStore = defineStore('club', {
-//   state: () => ({
-//     clubs: null,
-//     api_url: "http://localhost:3000/getAllClubData",
-//     count: 0
-//   }),
-//   async getData() {
-//     try {
-//       this.clubs = await api.get()
-//       console.log(this.clubs)
-//     } catch (error) {
-//       console.error(error)
-//       // let the form component display the error
-//       return error
-//     }
-//   },
-// })
-
 export const useClubStore = defineStore("club", {
   state: () => ({
     clubName: "",
