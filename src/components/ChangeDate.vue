6fd157d565e7a90e57cf225637b5c32d76c22de1
kevinc2465 <chenk5104@gmail.com>
2023-10-31 10:52:54 -0400

    deleted something useless

--- src/components/ChangeDate.vue
+++ src/components/ChangeDate.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <vue-changedate>
-      <input type="text" placeholder="New Date" v-model="nameInput" />
-      <button
-        class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-14 sm:w-auto"
-        @click="clubStore.changeData()"
-      >
-        Change date
-      </button>
-    </vue-changedate>
+    <input type="text" placeholder="New Date" v-model="nameInput" />
+    <button
+      class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-14 sm:w-auto"
+      @click="clubStore.changeData()"
+    >
+      Change date
+    </button>
   </div>
 </template>
 
