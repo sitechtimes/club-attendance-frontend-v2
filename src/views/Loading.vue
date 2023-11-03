8baf6a71fba032fb89d04e80cfdb12d0bc31cf23
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-11-03 10:59:55 -0400

    loading screen frame 1 janky

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,6 +1,20 @@
-<template class="bg-black">
-<div class="bg-black flex h-[100rem]">wjwwjwjw</div>
+<template>
+<div 
+class="bg-black 
+flex 
+w-screen h-screen flex-col justify-end items-center">
+<img
+src="@/assets/LoadingSeagull.png"
+alt="screenshot"
+class="w-204px h-87px"
+/>
+<svg xmlns="http://www.w3.org/2000/svg" 
+class="w-full h-1/2"
+viewBox="0 0 1200 360" fill="none">
+<path d="M600 360H1200L600 0L0 360H600Z" fill="#C2B669"/>
+</svg>
+</div>
+
 </template>
 <script setup lang="ts">
-
 </script>
