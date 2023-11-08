484b92ab8854e9bcda0e03009b01a403c32adaba
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-11-08 10:17:48 -0500

    loading screen progress

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,15 +1,23 @@
 <template>
 <div
-class="bg-black 
+class="
+bg-black 
 flex 
 w-screen h-screen flex-col justify-end items-center">
+<div>
+
+</div>
 <img
 src="@/assets/LoadingSeagull.png"
 alt="screenshot"
-class="w-204px h-87px"
+class="
+
+w-204px h-87px"
 />
 <svg xmlns="http://www.w3.org/2000/svg" 
-class="w-full h-360"
+class="
+
+w-1200px h-360px"
 viewBox="0 0 1200 360" fill="none">
 <path d="M600 360H1200L600 0L0 360H600Z" fill="#C2B669"/>
 </svg>
@@ -19,9 +27,22 @@ viewBox="0 0 1200 360" fill="none">
 <script setup lang="ts">
 </script>
 <style>
-@keyframes flight{
- 10% {
-    transform: translate3d(0, 10px, 0);
+.trail{
+ animation-name: expand;
+ animation-duration: 2s;
+ animation-iteration-count: infinite;
+}
+@keyframes expand{
+ 50% {
+    transform: translate3D(0, 100px, 0);
  }
+}
+.arrow-up {
+  width: 0; 
+  height: 0; 
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  
+  border-bottom: 5px solid black;
 }
 </style>
