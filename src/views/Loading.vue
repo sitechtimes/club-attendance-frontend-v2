72b895fa90eca2ba8c9f126d470c2c35387770c0
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-11-10 10:59:19 -0500

    figured out smooth svg animation

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,26 +1,31 @@
 <template>
 <div
 class="
+background
 bg-black 
 flex 
 w-screen h-screen flex-col justify-end items-center">
-<div
-class="arrow-up">
-
-</div>
 <img
 src="@/assets/LoadingSeagull.png"
 alt="screenshot"
 class="
-
-w-204px h-87px"
+trail
+w-204px h-auto"
 />
 <svg xmlns="http://www.w3.org/2000/svg" 
 class="
-trail
-w-1200px h-360px"
+w-100% h-auto"
 viewBox="0 0 1200 483" fill="none">
 <path d="M600 483H922.5H1094.5H1200V360L600 0L0 360V483H289H600Z" fill="#C2B669"/>
+<animateTransform
+      attributeName="transform"
+      attributeType="XML"
+      type="scale"
+      from="1 1"
+      to="5 5"
+      dur="5s"
+      repeatCount="1" 
+/>
 </svg>
 
 </div>
@@ -41,9 +46,14 @@ viewBox="0 0 1200 483" fill="none">
 }
 @keyframes colorChange{
   0%{
+    animation-timing-function: steps(1, end);
     background-color: black;
   }
-  50%{
+  45%{
+    
+    background-color: #C2B669;
+  }
+  70%{
     background-color: #C2B669;
   }
   100%{
@@ -51,16 +61,16 @@ viewBox="0 0 1200 483" fill="none">
   }
 }
 @keyframes expand{
- 50%{
-  background: black;
-  transform: translate3d(0, -40rem, 0)
+ 100%{
+  background-color: black;
+  transform: translate3d(0, -20rem, 0)
  }
 }
 .arrow-up {
   width: 0; 
   height: 0; 
-  border-left: 50px solid transparent;
-  border-right: 50px solid transparent;
+  border-left: 20rem solid transparent;
+  border-right: 20rem solid transparent;
   border-bottom: 50px solid white;
 }
 </style>
