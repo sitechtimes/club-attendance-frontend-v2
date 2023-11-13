ee59148644ea3c7ca48a5f026dcf29827f6d5698
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-11-13 10:55:08 -0500

    bird trail complete

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -4,7 +4,7 @@ class="
 background
 bg-black 
 flex 
-w-screen h-screen flex-col justify-end items-center">
+w-screen h-screen flex-col justify-end items-center overflow-hidden">
 <img
 src="@/assets/LoadingSeagull.png"
 alt="screenshot"
@@ -14,16 +14,17 @@ w-204px h-auto"
 />
 <svg xmlns="http://www.w3.org/2000/svg" 
 class="
-w-100% h-auto"
-viewBox="0 0 1200 483" fill="none">
+w-1200 
+h-360
+"
+viewBox="0 0 1200 360" fill="none">
 <path d="M600 483H922.5H1094.5H1200V360L600 0L0 360V483H289H600Z" fill="#C2B669"/>
 <animateTransform
       attributeName="transform"
-      attributeType="XML"
       type="scale"
       from="1 1"
-      to="5 5"
-      dur="5s"
+      to="8.5 8.5"
+      dur="6s"
       repeatCount="1" 
 />
 </svg>
@@ -36,7 +37,7 @@ viewBox="0 0 1200 483" fill="none">
 <style>
 .trail{
  animation-name: expand;
- animation-duration: 5s;
+ animation-duration: 6s;
  animation-iteration-count: 1;
 }
 .background{
@@ -49,7 +50,7 @@ viewBox="0 0 1200 483" fill="none">
     animation-timing-function: steps(1, end);
     background-color: black;
   }
-  45%{
+  50%{
     
     background-color: #C2B669;
   }
@@ -62,8 +63,7 @@ viewBox="0 0 1200 483" fill="none">
 }
 @keyframes expand{
  100%{
-  background-color: black;
-  transform: translate3d(0, -20rem, 0)
+  transform: translate(0px, -360px)
  }
 }
 .arrow-up {
