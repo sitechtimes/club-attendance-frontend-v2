bbe56b2e3f60c2da264025f7b7b459ca8ae8c62e
kevinc2465 <chenk5104@gmail.com>
2023-12-04 10:59:02 -0500

    starting figma for admin page

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    class="w-[55%] border-2 border-black h-[35%] rounded-full pl-2 ml-2"
+    class="w-[55%] border-2 border-black h-[35%] rounded-full pl-5"
     placeholder="Search"
   />
 </template>
