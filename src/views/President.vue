466c3fbf670d2f17bc6239b300197a2df1267f59
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2023-12-11 10:32:11 -0500

    s

--- src/views/President.vue
+++ src/views/President.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="h-screen">
-    <!-- <div class="h-8 bg-[#c2b669]">
-
-        </div> -->
-    <div
-      class="p-6 flex flex-col justify-evenly items-center gap-6 md:flex-row md:flex-wrap"
-    >
-      <ClubCard name="Anime Club" />
-      <ClubCard name="Robotics Club" />
-      <ClubCard name="Swimming Club" />
-      <ClubCard name="Table Tennis Club" />
+    <div class="p-6 flex flex-col justify-evenly items-center gap-6 md:flex-row md:flex-wrap">
+      <ClubCard v-for="club in userStore.userClubData.PresidentOf" :name="club"/>
     </div>
   </div>
 </template>
@@ -20,9 +12,5 @@
   import { useUserStore } from '@/stores/users'
 
   const userStore = useUserStore()
-onMounted(() => {
-  const userStore = useUserStore()
-  console.log(userStore.userClubData.PresidentOf)
 
-})
 </script>
