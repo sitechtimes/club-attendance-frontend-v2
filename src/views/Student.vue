4da3033ad8e9e273fc4c6a90ea801e01fb2879ff
kevinc2465 <chenk5104@gmail.com>
2023-12-11 10:59:28 -0500

    it's over

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -1,30 +1,31 @@
 <template>
-  <div class="w-screen">
-    <div class="relative bg-white h-96">
-      <div class="text-black text-5xl font-normal font-['Itim']">
-        Next Meeting
+  <div>
+    <div>
+      <div class="relative bg-white h-96">
+        <div class="text-black text-5xl font-normal font-['Itim']">
+          Next Meeting
+        </div>
+        <LogOut></LogOut>
       </div>
-      <LogOut></LogOut>
-    </div>
-    <div
-      class="bg-black w-[255rem] h-[180vh] -rotate-[60deg] -ml-[50rem]"
-    ></div>
-    <div class="fixed inset-0 z-10">
-      <div
-        class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
-      >
-        <div
-          class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-[50%]"
-        >
-          <div class="bg-[#FFD700] sm:p-4 sm:pb-4">
-            <div class="sm:flex sm:items-start">
-              <div class="text-center sm:ml-4 sm:mt-0 sm:text-left">
-                <h1>Next Meetings</h1>
-                <SearchBar></SearchBar>
+      <div class="justify-items-end bg-black w-[75%] h-[45rem] -rotate-[60deg]">
+        <div class="">
+          <div
+            class="flex min-h-full justify-end p-4 text-center rotate-[60deg] sm:items-center sm:p-0"
+          >
+            <div
+              class="rounded-lg bg-white text-left shadow-xl transition-all w-[25%]"
+            >
+              <div class="bg-[#FFD700] sm:p-4 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                  <div class="text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <h1>Next Meetings</h1>
+                    <SearchBar></SearchBar>
+                  </div>
+                </div>
               </div>
+              <div class="bg-gray-50 px-4 py-4 sm:flex sm:px-6"></div>
             </div>
           </div>
-          <div class="bg-gray-50 px-4 py-4 sm:flex sm:px-6"></div>
         </div>
       </div>
     </div>
