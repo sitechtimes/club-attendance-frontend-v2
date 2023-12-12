0a84235d560e4879b5397352d4d9bdbe3e9c6190
kevinc2465 <chenk5104@gmail.com>
2023-12-12 10:59:25 -0500

    making it look better

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -1,32 +1,24 @@
 <template>
   <div>
-    <div>
-      <div class="relative bg-white h-96">
-        <div class="text-black text-5xl font-normal font-['Itim']">
-          Next Meeting
-        </div>
-        <LogOut></LogOut>
+    <div class="relative bg-white">
+      <div class="text-black text-5xl font-normal font-['Itim']">
+        Next Meeting
       </div>
-      <div class="justify-items-end bg-black w-[75%] h-[45rem] -rotate-[60deg]">
-        <div class="">
+      <LogOut></LogOut>
+    </div>
+    <div class="bg-black w-[100%] h-[45rem] -rotate-[60deg]">
+      <div
+        class="bg-[#FFD700] py-[1%] px-[2%] flex justify-center-start space-x-[2%] w-[50%] ml-96 auto rotate-[60deg]"
+      >
+        <h1 class="text-black">Next Meetings</h1>
+        <SearchBar class="w-[50%]"></SearchBar>
+        <!-- <div
+          class="flex min-h-full justify-end p-4 text-center sm:items-center sm:p-0"
+        >
           <div
-            class="flex min-h-full justify-end p-4 text-center rotate-[60deg] sm:items-center sm:p-0"
-          >
-            <div
-              class="rounded-lg bg-white text-left shadow-xl transition-all w-[25%]"
-            >
-              <div class="bg-[#FFD700] sm:p-4 sm:pb-4">
-                <div class="sm:flex sm:items-start">
-                  <div class="text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <h1>Next Meetings</h1>
-                    <SearchBar></SearchBar>
-                  </div>
-                </div>
-              </div>
-              <div class="bg-gray-50 px-4 py-4 sm:flex sm:px-6"></div>
-            </div>
-          </div>
-        </div>
+            class="rounded-lg bg-white text-left shadow-xl transition-all w-[25%]"
+          ></div>
+        </div> -->
       </div>
     </div>
   </div>
