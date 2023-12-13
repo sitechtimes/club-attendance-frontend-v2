f1248fa1fbc2f9d9f36a2cf70dd71fe50b369da0
kevinc2465 <chenk5104@gmail.com>
2023-12-12 22:09:33 -0500

    made the background

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -1,34 +1,39 @@
 <template>
-  <div>
-    <div class="relative bg-white">
+  <div class="bg-white w-full h-screen flex justify-center">
+    <div class="w-[calc(50%-5rem)] h-full bg-[#F1F1F1]">
       <div class="text-black text-5xl font-normal font-['Itim']">
         Next Meeting
       </div>
+
       <LogOut></LogOut>
     </div>
-    <div class="bg-black w-[100%] h-[45rem] -rotate-[60deg]">
-      <div
-        class="bg-[#FFD700] py-[1%] px-[2%] flex justify-center-start space-x-[2%] w-[50%] ml-96 auto rotate-[60deg]"
-      >
+
+    <div class="page w-[calc(50%+5rem)] h-full">
+      <div class="bg-[#FFD700] py-[1%] px-[2%] w-full">
         <h1 class="text-black">Next Meetings</h1>
-        <SearchBar class="w-[50%]"></SearchBar>
+        <SearchBar></SearchBar>
         <!-- <div
           class="flex min-h-full justify-end p-4 text-center sm:items-center sm:p-0"
         >
           <div
             class="rounded-lg bg-white text-left shadow-xl transition-all w-[25%]"
-          ></div>
-        </div> -->
+          ></div> -->
+        <!-- </div> -->
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { useClubStore } from "../stores/club";
 import SearchBar from "@/components/SearchBar.vue";
 import LogOut from "@/components/LogOut.vue";
-
-const open = ref(false);
 </script>
+
+<style scoped>
+.page {
+  background: black;
+  border-left: 10rem solid #f1f1f1;
+  border-bottom: 58rem solid transparent;
+  box-sizing: border-box;
+}
+</style>
