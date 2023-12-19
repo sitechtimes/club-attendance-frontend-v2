e7e950ef4b090aa04c7fd144f7940a27d26a64ea
kevinc2465 <chenk5104@gmail.com>
2023-12-18 22:13:14 -0500

    made the buttons for the president page

--- src/components/PresidentButtons.vue
+++ src/components/PresidentButtons.vue
@@ -1,83 +1,25 @@
 <template>
-  <div class="flex flex-col">
-    <div
-      class="box flex flex-col items-end hover:scale-105 ease-in-out duration-500 cursor-pointer"
-      @click="open = true"
-    ></div>
-  </div>
-  <img
-    src="@/assets/coding.jpeg"
-    alt="coding"
-    class="h-[228px] rounded-t-[20px]"
-  />
-  <div
-    class="bg-black w-full flex flex-col items-center justify-center rounded-b-[20px] h-[4.5rem]"
-  >
-    <div class="text-[#c2b669] text-xl">{{ name }}</div>
-    <div class="text-[#c2b669] flex items-center"></div>
-  </div>
-  <div v-show="open">
-    <div
-      class="relative z-10"
-      aria-labelledby="modal-title"
-      role="dialog"
-      aria-modal="true"
+  <div class="bg-gray-50 flex-col w-[25%] px-4 py-4 sm:flex sm:px-6">
+    <button
+      type="button"
+      class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto"
+      @click="
+        qrCode = true;
+        open = true;
+      "
     >
-      <div
-        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-      ></div>
-      <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div
-          class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
-        >
-          <div
-            class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
-          >
-            <div class="bg-white px-4 pb-4 pt-5 bg-yellow sm:p-6 sm:pb-4">
-              <div class="sm:flex sm:items-start">
-                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  <h3
-                    class="text-base font-semibold leading-6 text-gray-900"
-                    id="modal-title"
-                  >
-                    {{ name }}
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <div class="bg-gray-50 px-4 py-4 sm:flex sm:px-6">
-              <button
-                type="button"
-                class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto"
-                @click="
-                  qrCode = true;
-                  open = false;
-                "
-              >
-                Generate QR Code
-              </button>
-              <button
-                type="button"
-                class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto"
-                @click="
-                  changeDate = true;
-                  open = false;
-                "
-              >
-                Add/Change meeting date
-              </button>
-              <button
-                type="button"
-                class="w-full my-1 justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold bg-red text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                @click="open = false"
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      Generate QR Code
+    </button>
+    <button
+      type="button"
+      class="w-full my-1 justify-center rounded-md px-3 bg-black hover:bg-slate-900 text-[#c2b669] py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto"
+      @click="
+        changeDate = true;
+        open = true;
+      "
+    >
+      Add/Change meeting date
+    </button>
   </div>
   <div v-show="qrCode">
     <div
@@ -86,9 +28,6 @@
       role="dialog"
       aria-modal="true"
     >
-      <div
-        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-      ></div>
       <div class="fixed inset-0 z-10 flex justify-center">
         <div
           class="flex w-full items-center justify-center p-4 text-center sm:p-0"
@@ -161,10 +100,7 @@ import { useClubStore } from "../stores/club";
 
 const clubStore = useClubStore();
 const nextMeeting = clubStore.nextMeeting;
-const props = defineProps({
-  name: String,
-  nextMeeting: String,
-});
+const props = defineProps({});
 console.log(nextMeeting);
 const open = ref(false);
 const qrCode = ref(false);
