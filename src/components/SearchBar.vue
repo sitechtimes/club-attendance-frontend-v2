df917544df265ab3a0599b52c8f22906c1b1fb25
kevinc2465 <chenk5104@gmail.com>
2023-12-19 11:00:28 -0500

    starting to make Import picture button

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,8 +1,11 @@
 <template>
+  <!-- <MagnifyingGlassIcon /> -->
   <input
     class="w-[60%] border-2 border-black h-[35%] rounded-full pl-5"
     placeholder="Search"
   />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { MagnifyingGlassIcon } from "@heroicons/vue/24/solid";
+</script>
