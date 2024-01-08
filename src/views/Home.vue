d748c635ff992cedc9a3577d13515d22bff3f5c3
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2024-01-08 10:56:42 -0500

    asda

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -95,7 +95,7 @@ onMounted(() => {
   userStore.userClubData = arrUserCookie.ClubData
   userStore.uid = arrUserCookie.uid
   userStore.userAuthority = arrUserCookie.role
-  if (userStore.userAuthority === "admin") {
+  if (userStore.userAuthority === "Admin") {
     userStore.getAllClubData(userStore.uid)
     setTimeout(function push() {routePush("admin")}, 1000)
   } else if (userStore.userAuthority === "Club President") {
