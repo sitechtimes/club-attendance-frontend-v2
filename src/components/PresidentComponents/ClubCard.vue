a97c22ccec4deb77050f3c1b14dd400f31d14f20
vanessalei411 <140737133+vanessalei411@users.noreply.github.com>
2024-03-04 10:35:04 -0500

    adwsfghgffdsfghjhgfdsdfghyhgfd

--- src/components/PresidentComponents/ClubCard.vue
+++ src/components/PresidentComponents/ClubCard.vue
@@ -1,51 +1,8 @@
 <template>
-  <div>
-  <!-- navbar -->
-  <nav class="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <!-- <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"> -->
-      <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Club Attendance
-      </span>
-      <!-- </a> -->
-      <!-- log out button -->
-      <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-
-      <!-- hamburger menu -->
-    <button xmlns="" class="mr-10 text-white" type="button">
-      <img src="@/assets/menu.png" alt="" class="w-10">
-    </button>
-
-    <!-- Dropdown menu -->
-    <div id="dropdownDots" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
-          <li>
-            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-          </li>
-        </ul>
-        <div class="py-2">
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated link</a>
-        </div>
-    </div>
-
-<!-- logout button -->
-      <button type="button" class="text-black bg-yellow hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log Out
-      
-      </button>
-      </div>
-    </div>
-
-  </nav>
-
-  <div class="bg-gray w-1/6 h-1/2">
-<a href=""> hi </a>
-</div>
+  <div class="bg-gray w-full h-1/6">
+    <img src="./assets/testimg.png" alt="">
+    <a href=""> hi </a>
+  </div>
 
  <!-- club div -->
   <!-- <div class="flex flex-col">
@@ -207,7 +164,6 @@
         </div>
       </div>
     </div> -->
-  </div>
 </template>
 
 <!-- <script setup lang="ts">
