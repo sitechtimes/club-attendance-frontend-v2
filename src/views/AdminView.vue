3a1d288bafdc46e23eabd2be42e318d5ff14b6e0
kevinc2465 <chenk5104@gmail.com>
2024-03-25 11:00:05 -0400

    HAHAHAHHA :anger:

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -72,7 +72,14 @@
                     </div>
                   </div>
                 </div>
-                <div class="flex flex-row justify-evenly h-100vh">
+                <div
+                  class="flex flex-row justify-evenly h-100vh"
+                  @click="
+                    approveImage = true;
+                    open = false;
+                  "
+                >
+                  >
                   <div
                     v-for="image in userStore.unapprovedImages"
                     class="flex flex-col w-[30%]"
@@ -120,6 +127,41 @@
       </div>
     </section>
   </div>
+  <div v-show="approveImage">
+    <div
+      class="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+      ></div>
+      <div class="fixed inset-0 z-10 flex justify-center">
+        <div
+          class="flex w-full items-center justify-center p-4 text-center sm:p-0"
+        >
+          <div
+            class="flex flex-col px-6 py-4 sm:flex transform overflow-hidden rounded-lg items-center justify-center bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+          >
+            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <approveImage />
+            </div>
+            <button
+              type="button"
+              class="w-full my-1 rounded-md bg-red-600 px-3 bg-red py-2 text-sm font-semibold text-white shadow-sm hover:red-500 sm:ml-3 sm:w-fill sm:h-10"
+              @click="
+                approveImage = false;
+                open = true;
+              "
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -129,6 +171,7 @@ import { useUserStore } from "@/stores/users";
 import { useClubStore } from "@/stores/club";
 import { useRouter } from "vue-router";
 import { BellIcon } from "@heroicons/vue/24/solid";
+import approveImage from "@components/AdminComponents/approveImage.vue";
 
 const query = ref("");
 let open = ref(false);
