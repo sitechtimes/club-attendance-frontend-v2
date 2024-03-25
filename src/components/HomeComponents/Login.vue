d0bf3029d3081dcff074c62046434c5a5f1d2b72
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2024-03-25 11:09:16 -0400

    sso popup works?

--- src/components/HomeComponents/Login.vue
+++ src/components/HomeComponents/Login.vue
@@ -1,8 +1,7 @@
 <template>
   <div
     class="p-4 text-[#c2b669] shadow-xl cursor-pointer rounded-md hover:scale-110 ease-in-out duration-300 bg-black w-36 h-18 flex justify-evenly items-center"
-    @click="logIn"
-  >
+    @click="logIn">
     <div>Login</div>
     <ArrowLeftOnRectangleIcon class="w-8" />
   </div>
@@ -14,7 +13,12 @@ import { ArrowLeftOnRectangleIcon } from "@heroicons/vue/24/outline";
 const userStore = useUserStore();
 
 async function logIn() {
-  await userStore.googleLink();
+  // await userStore.googleLink();
+  try {
+    window.location.href = 'http://localhost:8000/o/authorize/?response_type=code&client_id=xzCx3rgs07o8C0uvil1hhkaXejsbE370aBci2Bb4&redirect_uri=http%3A%2F%2Flocalhost%3A5173&code_challenge=yJaxHcB8M4d_dj7ApOPFNLIkPV2Bk8N82tnl-PaGLDk&code_challenge_method=S256'
+  } catch (error) {
+    console.log(error)
+  }
 }
 </script>
 @/stores/users
