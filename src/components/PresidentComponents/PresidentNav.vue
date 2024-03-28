93a49c50f6cc78b031ceb417f5ddda976a6df52a
vanessalei411 <140737133+vanessalei411@users.noreply.github.com>
2024-03-28 10:56:52 -0400

    wdawdawawda

--- src/components/PresidentComponents/PresidentNav.vue
+++ src/components/PresidentComponents/PresidentNav.vue
@@ -1,5 +1,4 @@
 <template>
-
     <nav class="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <!-- <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -11,27 +10,15 @@
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 
       <!-- hamburger menu -->
-    <button xmlns="" class="mr-10 text-white" type="button">
+      <div> 
+    <button  @click="show = !show" class="mr-10 inline-flex gap-x-1.5" type="button">
       <img src="@/assets/menu.png" alt="" class="w-10">
     </button>
-
-    <!-- Dropdown menu -->
-    <div id="dropdownDots" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
-          <li>
-            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-          </li>
-        </ul>
-        <div class="py-2">
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated link</a>
-        </div>
     </div>
+
+  <Transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95"/>
+   
+  
 
 <!-- logout button -->
       <button type="button" class="text-black bg-yellow hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log Out
@@ -43,7 +30,23 @@
   </nav>
 </template>
 
-<script>
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+import { Transition } from 'vue'
 
-
+var show = false;
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
+
