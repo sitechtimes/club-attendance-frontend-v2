2338416ac6fc713421353810ae8a399970bb17c4
vanessalei411 <140737133+vanessalei411@users.noreply.github.com>
2024-04-08 10:57:53 -0400

    aaasfgfds

--- src/components/PresidentComponents/PresidentNav.vue
+++ src/components/PresidentComponents/PresidentNav.vue
@@ -1,29 +1,43 @@
 <template>
+  <div>
     <nav class="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Club Attendance
       </span>
       <!-- log out button -->
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-
-      <!-- hamburger menu -->
-      <div id="demo"> 
-    <button @click="show = !show">
-      <img src="@/assets/menu.png" alt="" class="w-10">
-    </button>
-    <Transition  name="fade"> 
-       <p v-if="show" class="text-white">hello</p>
-    </Transition>
-    </div>
-
 <!-- logout button -->
       <button type="button" class="text-black bg-yellow hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log Out
       
       </button>
+
+      <button @click="show = !show">
+        <img src="@/assets/menu.png" alt="" class="w-10">
+      </button>
+
       </div>
     </div>
 
   </nav>
+  
+      <!-- hamburger menu -->
+      <div id="demo"> 
+        <Transition v-if="show" name="fade"> 
+          <div>
+           <p v-if="show" class="text-white bg-white h-auto w-10 max-w-screen-xl mx-auto p-4" style="margin-top: 64px; z-index: 10;">hello</p>
+          <ul> 
+            <li class="hamburger-item hover:bg-sky-white">
+              
+            </li>
+          </ul>
+    
+    
+        </div>
+        </Transition>
+        </div>
+
+      </div>
+    
 </template>
 
 <script setup lang="ts">
