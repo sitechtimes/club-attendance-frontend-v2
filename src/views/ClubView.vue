45c147617fadb7ca8cf2a75fc3e8bd3dfa3442bc
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2024-04-08 10:55:31 -0400

    very sleepy :(

--- src/views/ClubView.vue
+++ src/views/ClubView.vue
@@ -6,32 +6,41 @@
     <NoPermsPageGuard />
   </div>
   <div v-else-if="store.user.isAuthenticated &&
-    store.user.role == 'Admin'">
-    
+    store.user.role == 'Admin'"> 
     <div class="w-screen h-auto flex flex-col">
-      <div class="w-full h-[30vh] flex flex-row border-b-[0.15rem] overflow-hidden">
-       <div>
-        <img
+      <div class="w-full h-[30vh] flex flex-row 
+      border-b-[0.15rem] overflow-hidden">
+      <div>
+         <img
              src="@/assets/sammy.jpg"
              alt="Sammy the Seagull"
              class="w-[300px] h-[30vh]"
-        />
+         />
         <!-- club photo placeholder -->
-        </div>
-        <div class="w-[30%] h-[5vh] text-2xl border-black p-1
-        pb-[2.5rem] pl-6 border-b-[0.15rem] text-gold text-clip
-        text-drop-shadow-[0_1.2px_1.2px_rgba(0,0,0)]">{{ clubStore.clubName }}</div>
-        <UserIcon class="h-[8vh]  border-l-[0.15rem] border-b-[0.15rem]"></UserIcon>
-        <div class=" text-2xl border-b-[0.15rem] h-[8vh]"> Room {{ clubStore.room }}</div>
+      </div>
+      <div class="flex flex-col">
+        <div class="flex flex-row">
+         <div class="w-[30vw] h-[10vh] text-2xl border-black p-1
+         pb-[2.5rem] pl-6 border-b-[0.15rem] text-gold text-clip
+         text-drop-shadow-[0_1.2px_1.2px_rgba(0,0,0)]">{{ clubStore.clubName }}</div>
+         <UserIcon class="h-[12vh]  border-l-[0.15rem] border-b-[0.15rem]"></UserIcon>
+         <div class=" text-2xl border-b-[0.15rem] h-[12vh]"> Room {{ clubStore.room }}</div>
         <RouterLink to="/admin" class="font-normal text-[2rem] 
-        p-1 h-[8vh] border-b-[0.15rem] flex flex-row-reverse">Admin</RouterLink>
+        w-[10vw] p-1 h-[12vh] border-b-[0.15rem] flex flex-row-reverse">Admin</RouterLink>
+       </div>
+       <div class="flex flex-row">
+        <div class="flex-col w-[30vw] pl-6">
+          <div>Advisor: {{ clubStore.clubAdvisor }}</div>
+          <div>President: {{ clubStore.clubPresident }}</div>
+        </div>
+
+       </div>
+      </div>
       </div>
       <div class="w-full flex flex-col justify-start items-center pt-3">
         <a href="https://www.youtube.com/watch?v=RbhVgdDoY3Q" target="_blank"
           class="bg-black h-[7vh] w-[50vh] flex flex-col items-center justify-center rounded-full text-[#fff]">Photo
           Attendance Link</a>
-        <p>Advisor: {{ clubStore.clubAdvisor }}</p>
-        <p>President: {{ clubStore.clubPresident }}</p>
       </div>
        <table class="table-auto border-collapse border border-slate-400 w-[80vw] m-auto overflow-auto">
   <thead>
