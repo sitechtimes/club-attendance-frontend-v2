3d059b644eb0329eafe1fab311569ae423f662da
vanessalei411 <140737133+vanessalei411@users.noreply.github.com>
2024-04-09 10:31:02 -0400

    sdfg;['

--- src/components/PresidentComponents/PresidentNav.vue
+++ src/components/PresidentComponents/PresidentNav.vue
@@ -6,8 +6,8 @@
       </span>
       <!-- log out button -->
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-<!-- logout button -->
-      <button type="button" class="text-black bg-yellow hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log Out
+
+        <button type="button" class="text-black fixed bg-yellow hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log Out
       
       </button>
 
@@ -21,9 +21,9 @@
   </nav>
   
       <!-- hamburger menu -->
-      <div id="demo"> 
+      <div id="demo" class="relative"> 
         <Transition v-if="show" name="fade"> 
-          <div>
+          <div class="absolute round-lg">
            <p v-if="show" class="text-white bg-white h-auto w-10 max-w-screen-xl mx-auto p-4" style="margin-top: 64px; z-index: 10;">hello</p>
           <ul> 
             <li class="hamburger-item hover:bg-sky-white">
